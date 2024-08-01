88314b7c83ce72d8df9cb2e2e9ff1d8c8a74bb5a
pyyupsk <pyyupsk@proton.me>
2024-08-01 22:48:03 +0700

    fix(admin-layout): correct redirect for non-admin users
    - Modified redirect in admin layout to properly return the function.

--- src/app/(administrator)/layout.tsx
+++ src/app/(administrator)/layout.tsx
@@ -8,7 +8,7 @@ export default function Layout({ children }: { children: ReactNode }) {
     const { sessionClaims } = auth();
 
     if (!sessionClaims?.metadata?.role || sessionClaims.metadata.role !== 'admin') {
-        redirect('/');
+        return redirect('/');
     }
 
     return (
