23602bd5af134bbd3448c16c3da18750b35557d6
pyyupsk <pyyupsk@proton.me>
2024-08-01 17:22:44 +0700

    refactor: streamline achievements data fetching and rendering
    - Consolidated achievements fetching logic to reduce redundancy
    - Simplified handling of test data and production data
    - Updated mapping logic to use chievement.id as key

--- src/app/(main)/components/members-achievements.tsx
+++ src/app/(main)/components/members-achievements.tsx
@@ -3,7 +3,6 @@ import { CopyButton } from '@/components/copy-button';
 import { buttonVariants } from '@/components/ui/button';
 import { env } from '@/env';
 import { prisma } from '@/lib/prisms';
-import { Achievements } from '@prisma/client';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -12,33 +11,36 @@ type Props = {
 };
 
 export async function MembersAchievements({ limit = undefined }: Props) {
-    const achievements: Omit<Achievements, 'created_at'>[] = [];
-
-    if (env.NODE_ENV === 'production') {
-        const result = await prisma.achievements.findMany({
-            orderBy: { id: 'asc' },
-            take: limit ? limit : undefined,
-        });
-
-        achievements.push(...result);
-    }
-
-    for (let i = 0; i < (limit ? limit : randomInt(5, 20)); i++) {
-        achievements.push({
-            id: `test-${i}`,
-            title: 'title',
-            description: 'description',
-            image: 'https://placeholder.com/500x500',
-            member: `member-${i}`,
-        });
-    }
+    const achievements =
+        env.NODE_ENV === 'production'
+            ? await prisma.achievements.findMany({
+                  orderBy: { id: 'asc' },
+                  take: limit,
+                  select: {
+                      id: true,
+                      title: true,
+                      description: true,
+                      image: true,
+                      member: true,
+                  },
+              })
+            : Array.from({ length: limit ?? randomInt(5, 20) }, (_, i) => ({
+                  id: `test-${i}`,
+                  title: 'title',
+                  description: 'description',
+                  image: 'https://placeholder.com/500x500',
+                  member: `member-${i}`,
+              }));
 
     return (
         <section className="py-16 container">
             <h2 className="text-center">ความสำเร็จของสมาชิก</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 my-8">
-                {achievements.map((achievement, index) => (
-                    <div key={index} className="rounded-t-lg border-b border-primary shadow-lg transform relative">
+                {achievements.map((achievement) => (
+                    <div
+                        key={achievement.id}
+                        className="rounded-t-lg border-b border-primary shadow-lg transform relative"
+                    >
                         <CopyButton text={achievement.id} className="absolute top-2 right-2" />
                         <Image
                             src={achievement.image}
