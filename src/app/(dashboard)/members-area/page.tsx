46f4aa2ac276b174112424012dc59976ee1bf7ef
pyyupsk <pyyupsk@proton.me>
2024-08-01 22:48:15 +0700

    feat(members-area): update members area page with development notice
    - Added a placeholder message for members area indicating ongoing development.
    - Included a button to navigate back to the homepage.

--- src/app/(dashboard)/members-area/page.tsx
+++ src/app/(dashboard)/members-area/page.tsx
@@ -1,3 +1,18 @@
+import { buttonVariants } from '@/components/ui/button';
+import Link from 'next/link';
+
 export default function Page() {
-    return <div>Members Area</div>;
+    return (
+        <div className="flex flex-col items-center justify-center text-center min-h-screen max-w-3xl mx-auto">
+            <h2>หน้าเพจสำหรับสมาชิกกำลังอยู่ในระหว่างพัฒนา</h2>
+            <p className="leading">
+                เรากำลังทำงานอย่างหนักเพื่อสร้างหน้าเพจสำหรับสมาชิกให้ดียิ่งขึ้น ติดตามความคืบหน้าและคุณสมบัติใหม่ๆ
+                ที่จะมาเสริมสร้างประสบการณ์การเล่นเกมและการมีส่วนร่วมในชุมชนของคุณ
+            </p>
+            <p className="leading">โปรดอดใจรอ และขอบคุณสำหรับความอดทนของคุณ!</p>
+            <Link href="/" className={buttonVariants({ size: 'lg', className: 'mt-4' })}>
+                กลับสู่หน้าหลัก
+            </Link>
+        </div>
+    );
 }
